e6516a058a127c39c7658cd1ef86618424665ab2
Felix Schulenburg <felix.schulenburg@gmail.com>
2020-09-01 20:15:57 +0200

    Added GET to find album using Spotify's search API

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+
+var app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4444;
+const CLIENT_ID = process.env.CLIENT_ID || null;
+const CLIENT_SECRET = process.env.CLIENT_SECRET || null;
+
+app.get('/:album', (req, res) => {
+    if (!CLIENT_ID) res.error(403).send('No client ID available.');
+    if (!CLIENT_SECRET) res.error(403).send('No client secret available.');
+
+    axios({
+        url: 'https://accounts.spotify.com/api/token',
+        method: 'post',
+        params: {
+            grant_type: 'client_credentials'
+        },
+        headers: {
+            'Accept':'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        auth: {
+            username: CLIENT_ID,
+            password: CLIENT_SECRET
+        }
+    }).then(function(response) {
+        return axios({
+            url: `https://api.spotify.com/v1/search?q=${req.params.album}&type=album`,
+            method: 'get',
+            headers: {
+                'Authorization': `${response.data.token_type} ${response.data.access_token}`,
+                "Accept": "application/json",
+                'Content-Type': 'application/json'
+            }
+        });
+    }).then(function(response) {
+        res.send(response.data);
+    }).catch(function(error) {
+        res.error(400).send(error);
+    });
+});
+
+app.listen(PORT, () => console.log(`Running node app on port ${PORT}.`));
